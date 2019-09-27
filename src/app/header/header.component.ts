7d01afd13bc2e31fb762777675d88cc837d3ea3b
dheeraj sharma <dj4385@gmail.com>
2019-09-27 12:27:54 +0530

    header and dashboard page is done

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  userInfo = {
+    name : "",
+    email : "",
+    token : ""
+  }
+  
   constructor() { }
 
   ngOnInit() {
+    this.getLoginUserInfo()
+  }
+  getLoginUserInfo(){
+    this.userInfo.name = localStorage.getItem('name')
+    this.userInfo.email = localStorage.getItem('email')
+    this.userInfo.token = localStorage.getItem('token')
   }
 
 }
