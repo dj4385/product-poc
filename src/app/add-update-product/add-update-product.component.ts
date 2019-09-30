4aa3840fa19b8a0628bef873fa2656275c15cb7c
dheeraj sharma <dj4385@gmail.com>
2019-09-30 11:09:40 +0530

    update api is under process

--- src/app/add-update-product/add-update-product.component.ts
+++ src/app/add-update-product/add-update-product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonSerService } from '../common/common-ser.service';
 import { AlertSerService } from '../common/alert-ser.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-update-product',
@@ -11,19 +13,42 @@ export class AddUpdateProductComponent implements OnInit {
 
   productDetailObj : any = {}
   response : any = {}
+  responseArr : any = []
   token = ""
+  id = ""
   isProductDetailObj = false
 
 
   constructor(
     private _comSer: CommonSerService,
-    private _alert: AlertSerService
+    private _alert: AlertSerService,
+    private _activeRoute: ActivatedRoute,
+    private _router : Router
   ) { }
 
   ngOnInit() {
+    this.id = this._activeRoute.snapshot.queryParams['id']
     this.token = localStorage.getItem("token")
-    if(Object.entries(this.productDetailObj).length === 0){
+    console.log("id",this.id)
+    if(this.id === undefined){
       this.isProductDetailObj = true
+    } else {
+      this._comSer.getProducts(this.token).subscribe(
+        res=>{
+          this.response = res
+          this.responseArr = this.response._products
+          this.responseArr.forEach(element => {
+            if(element._id === this.id){
+              this.productDetailObj.productName = element.productName
+              this.productDetailObj.productQty = element.productQty
+              this.productDetailObj.price = element.price
+            }
+          });
+        },
+        err=>{
+          console.log(err)
+        }
+      )
     }
   }
 
@@ -44,10 +69,23 @@ export class AddUpdateProductComponent implements OnInit {
     }
   }
   updateProduct(){
-
+    if(this.token){
+      this._comSer.updateProduct(this.id, this.productDetailObj, this.token).subscribe(
+        res=>{
+          this.response = res
+          this._alert.successMsg(this.response.message)
+          this.reset()
+          this._router.navigate(['/dashboard'])
+        },
+        err=>{
+          this._alert.errorMsg(err.message)
+        }
+      )
+    } else {
+      this._alert.errorMsg("Invalid token")
+    }
   }
   reset(){
     this.productDetailObj = {}
   }
-
 }
