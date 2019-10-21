a34039fdb6b1f8e3f20c76dba54b2105fb79265d
dj4385 <dj4385@gmail.com>
2019-10-21 21:22:27 +0530

    production url

--- src/app/common/common-ser.service.ts
+++ src/app/common/common-ser.service.ts
@@ -1,54 +1,44 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment } from '../../environments/environment'
+import { environment } from '../../environments/environment.prod'
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommonSerService{
-  apiUrl = {
-    "registerUrl": "/v1/api/register",
-    "loginUrl": "/v1/api/login",
-    "getProductUrl" : "/v1/api/product/products",
-    "addProductUrl" : "/v1/api/product/addProduct",
-    "deleteProductUrl": "/v1/api/product/products/",
-    "updateProductUrl": "/v1/api/product/products/",
-    "getSingleProductUrl": "/v1/api/product/product/",
-    "forgetPasswordUrl": "/v1/api/forgetPassword"
-  }
-
+  
   constructor(
     private _httpClient: HttpClient
   ) { }
 
   registerUser(registerUserObj){
-    return this._httpClient.post(this.apiUrl.registerUrl, registerUserObj)
+    return this._httpClient.post(environment.apiUrl.registerUrl, registerUserObj)
   }
   loginUser(loginUserObj){
-    return this._httpClient.post(this.apiUrl.loginUrl, loginUserObj)
+    return this._httpClient.post(environment.apiUrl.loginUrl, loginUserObj)
   }
   getProducts(token){
     let _header = new HttpHeaders({"token":token})
-    return this._httpClient.get(this.apiUrl.getProductUrl, { headers: _header})
+    return this._httpClient.get(environment.apiUrl.getProductUrl, { headers: _header})
   }
   getSingleProduct(id, token){
     let _header = new HttpHeaders({"token":token})
-    return this._httpClient.get(this.apiUrl.getSingleProductUrl+id, { headers: _header})
+    return this._httpClient.get(environment.apiUrl.getSingleProductUrl+id, { headers: _header})
   }
   addProduct(productObj, token){
     let _header = new HttpHeaders({"token": token})
-    return this._httpClient.post(this.apiUrl.addProductUrl, productObj, {headers: _header})
+    return this._httpClient.post(environment.apiUrl.addProductUrl, productObj, {headers: _header})
   }
   updateProduct(id, productObj, token){
     let _header = new HttpHeaders({"token": token})
-    return this._httpClient.put(this.apiUrl.updateProductUrl+id, productObj, {headers: _header})
+    return this._httpClient.put(environment.apiUrl.updateProductUrl+id, productObj, {headers: _header})
   }
   deleteProduct(id,token){
     let _header = new HttpHeaders({"token":token})
-    return this._httpClient.delete(this.apiUrl.deleteProductUrl+id, { headers: _header})
+    return this._httpClient.delete(environment.apiUrl.deleteProductUrl+id, { headers: _header})
   }
   forgetPassword(emailObj){
-    return this._httpClient.post(this.apiUrl.forgetPasswordUrl, emailObj)
+    return this._httpClient.post(environment.apiUrl.forgetPasswordUrl, emailObj)
   }
 
   isLogin(){
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: {
+      registerUrl: "https://product-poc-1.herokuapp.com/api/register",
+      loginUrl: "https://product-poc-1.herokuapp.com/api/login",
+      getProductUrl : "https://product-poc-1.herokuapp.com/api/product/products",
+      addProductUrl : "https://product-poc-1.herokuapp.com/api/product/addProduct",
+      deleteProductUrl: "https://product-poc-1.herokuapp.com/api/product/products/",
+      updateProductUrl: "https://product-poc-1.herokuapp.com/api/product/products/",
+      getSingleProductUrl: "https://product-poc-1.herokuapp.com/api/product/product/",
+      forgetPasswordUrl: "https://product-poc-1.herokuapp.com/api/forgetPassword"
+    }
 };
